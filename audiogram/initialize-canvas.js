c814d483b32cb75f31214086d2ae28ee9549ac6f
Michael Borisov <michael.g.borisov@gmail.com>
2018-04-05 23:01:02 +0300

    Download image from HTTPS

--- audiogram/initialize-canvas.js
+++ audiogram/initialize-canvas.js
@@ -1,10 +1,23 @@
 var fs = require("fs"),
-    path = require("path"),
-    Canvas = require("canvas"),
-    getRenderer = require("../renderer/");
+  path = require("path"),
+  Canvas = require("canvas"),
+  getRenderer = require("../renderer/"),
+  https = require('https');
+
+var download = function (url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = https.get(url, function (response) {
+    response.pipe(file);
+    file.on('finish', function () {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function (err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
 
 function initializeCanvas(theme, cb) {
-
   // Fonts pre-registered in bin/worker
   var renderer = getRenderer(theme);
 
@@ -13,20 +26,25 @@ function initializeCanvas(theme, cb) {
   }
 
   // Load background image from file (done separately so renderer code can work in browser too)
-  fs.readFile(path.join(__dirname, "..", "settings", "backgrounds", theme.backgroundImage), function(err, raw){
+  if (theme.backgroundImage.startsWith("https:")) {
+    var ext = theme.backgroundImage.substr(theme.backgroundImage.lastIndexOf('.') + 1);
+    var fileName = "file." + ext;
+    var filePath = path.join(__dirname, "..", "settings", "backgrounds", fileName);
 
-    if (err) {
-      return cb(err);
-    }
+    download(theme.backgroundImage, filePath, function () {
+      fs.readFile(filePath, function (err, raw) {
+        if (err) {
+          return cb(err);
+        }
 
-    var bg = new Canvas.Image;
-    bg.src = raw;
-    renderer.backgroundImage(bg);
+        var bg = new Canvas.Image;
+        bg.src = raw;
+        renderer.backgroundImage(bg);
 
-    return cb(null, renderer);
-
-  });
-
+        return cb(null, renderer);
+      });
+    });
+  }
 }
 
 module.exports = initializeCanvas;
